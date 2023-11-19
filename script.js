c08f0f199c02137814579d66da1f4b80ceb55d28
Taha Ali Khan <49469972+tahaalikhan123@users.noreply.github.com>
2023-11-18 19:19:46 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Example dynamic content
+const content1 = "Dynamic Content for Container 1";
+const content2 = "Dynamic Content for Container 2";
+const content3 = "Dynamic Content for Container 3";
+const content4 = "Dynamic Content for Container 4";
+
+// Update container content
+document.getElementById("container1").innerHTML = content1;
+document.getElementById("container2").innerHTML = content2;
+document.getElementById("container3").innerHTML = content3;
+document.getElementById("container4").innerHTML = content4;
